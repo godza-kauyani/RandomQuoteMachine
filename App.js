d5e3507c27776b49bda8f328e1c591a43d07cb31
godza-kauyani <kauyanigodfrey@gmail.com>
2019-02-27 14:10:05 +0200

    load to github

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import QuotesBox from './Components/QuotesBox/QuotesBox';
-import {Container } from 'reactstrap';
-import { quotes } from './quotes';
-
+import {Container} from 'reactstrap';
+import {quotes} from './quotes';
 
 class App extends Component {
-//initial state of the component
-	state={quote:''};
-//to be used by the component
-	onClick =()=>{
-		this.getQuote();
-	}
-	getQuote =()=>{
-		let quote = quotes[Math.floor(Math.random()*quotes.length)]
-		this.setState({quote:quote});
-	}
+  //initial state of the component
+  state = {quote: ''};
+  //to be used by the component
+  onClick = () => {
+    this.getQuote();
+  };
+  getQuote = () => {
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+    this.setState({quote: quote});
+  };
 
-	componentDidMount(){
-		this.getQuote();
-	}
-	
+  componentDidMount() {
+    this.getQuote();
+  }
+
   render() {
     return (
-	    <Container>
-		    <span>{this.state.quote.quote}</span>
-		    <span>{this.state.quote.author}</span>
-		    <QuotesBox quote={this.state.quote}
-			    id='quotes-box'
-		    	    onClick={this.onClick}
-		    />
-    	     </Container>
+      <Container>
+        <QuotesBox
+          quote={this.state.quote}
+          onClick={this.onClick}
+        />
+      </Container>
     );
   }
 }
